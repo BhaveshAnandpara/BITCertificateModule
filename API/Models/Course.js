fb6bebc87987f77a1be192256938a739b81ce1e0
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-02-25 19:03:25 +0530

    Course Models Changed

--- API/Models/Course.js
+++ API/Models/Course.js
@@ -6,12 +6,12 @@ const CourseSchema = mongoose.Schema({
     courseId : {type : String , required : true },
     courseName : { type : String , required : true},
     courseShortForm : { type : String , required : false},
-    courseType : { type : Array , enum : config.courseType , required : true },
+    courseType : { type : String  , enum :{ values :  config.courseType , message : "Invalid CourseType" } , required : true },
     createdBy : { type : mongoose.Schema.Types.ObjectId , ref : 'Faculty' , required : true },
     assignedTo : { type : mongoose.Schema.Types.ObjectId , ref : 'Faculty' , required : true },
     academicSession : {type : String , required : true },
-    department : { type : String , enum : config.departments , required : true},
-    certificateTemplate : { type : Blob ,  required : true},
+    department : { type : String , required : true},
+    certificateTemplate : { type : String ,  required : true},
 
 })
 
